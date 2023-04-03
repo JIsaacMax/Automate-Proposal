ed037176400ce11a1fde5197d144241696c41182
Isaac Max <j.isaacmaximo@gmail.com>
2023-04-03 16:35:04 -0300

    Commit Error

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,15 +1,17 @@
-function formatDate(dateString) {
-  const date = new Date(dateString);
-  const day = String(date.getDate()).padStart(2, "0");
-  const month = String(date.getMonth() + 1).padStart(2, "0");
-  const year = date.getFullYear();
-  return `${day} · ${month} · ${year}`;
-}
+const form = document.getElementById('proposalForm');
+    const preview = document.getElementById('preview');
 
-function formatCurrency(value) {
-  return `R$${parseFloat(value).toFixed(2).replace(".", ",")}`;
-}
+    form.addEventListener('input', () => {
+      const name = document.getElementById('name').value;
+      const date = document.getElementById('date').value;
+      const company = document.getElementById('company').value;
+      const logo = document.getElementById('logo').value;
+      const values = document.getElementById('values').value;
+      const services = document.getElementById('services').value;
+      const scope = document.getElementById('scope').value;
+      const testimonials = document.getElementById('testimonials').value;
 
+<<<<<<< HEAD
 function updatePreview() {
   document.getElementById("proposal-type").textContent =
     name || "Estratégia de Marketing";
@@ -92,3 +94,21 @@ function tradeValues(tag){
     imageTrade.src = "/src/img/Preview4IMG.jpg";
   }
 }
+=======
+      preview.innerHTML = `
+        <div class="header">
+          <h2>${company}</h2>
+          <img src="${logo}" alt="${company} logo" class="logo" style="" />
+        </div>
+        <p>Nome: ${name}</p>
+        <p>Data: ${date}</p>
+        <p>Valores: ${values}</p>
+        <h3>Serviços:</h3>
+        <p>${services}</p>
+        <h3>Escopo:</h3>
+        <p>${scope}</p>
+        <h3>Depoimentos:</h3>
+        <p>${testimonials}</p>
+      `;
+    });
+>>>>>>> parent of c316f43 (Update)
